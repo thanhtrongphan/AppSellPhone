a7742a7e88186f945d2addd0f208e4b50dcf1c0d
Phan Thanh Trọng <124451210+thanhtrongphan@users.noreply.github.com>
2024-03-24 21:28:33 +0700

    Update firebase

--- Source/app/src/main/java/com/example/appsellphone/MainActivity.java
+++ Source/app/src/main/java/com/example/appsellphone/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.appsellphone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_1);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Categories/Category_id_1/name");
+        myRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                textView.setText(task.getResult().getValue().toString());
+            } else {
+                textView.setText("Failed to read value.");
+            }
+        });
     }
 }
